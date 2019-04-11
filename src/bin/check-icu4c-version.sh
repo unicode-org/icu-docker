1f31b84a5cbbcbe6471ee17bdc5eb23078f774a0
Steven R. Loomis <srloomis@us.ibm.com>
2019-04-11 12:17:47 -0700

    update for review comment
    - also add a workaround for icu 58/59
    
    Fixes: https://github.com/unicode-org/icu-docker/issues/12

--- src/bin/check-icu4c-version.sh
+++ src/bin/check-icu4c-version.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
+# © 2016 and later: Unicode, Inc. and others.
+# License & terms of use: http://www.unicode.org/copyright.html
+
 if [ $# -ne 0 ];
 then
     cd $1
     shift
 fi
 
-if [ ! -f  ./source/common/unicode/uversion.h ];
+if [ ! -f  source/common/unicode/uversion.h ];
 then
     echo "unknown-no-uversion"
     exit 1
 fi
-( grep -h "^#define U_ICU_VERSION " ./source/common/unicode/uvernum.h || echo unknown-no-define) | cut -d' ' -f3 | tr -d '"'
+( grep "^#define U_ICU_VERSION " source/common/unicode/uvernum.h || echo unknown-no-define) | cut -d' ' -f3 | tr -d '"'
 exit 0
--- src/bin/icu-configure.sh
+++ src/bin/icu-configure.sh
@@ -3,4 +3,19 @@
 # License & terms of use: http://www.unicode.org/copyright.html
 
 . /src/bin/icu-dev.sh
-exec ${SHELL} /src/icu/icu4c/source/configure $*
+if [ $ICU4CMAJ = 58 ];
+then
+    # workaround for xlocale
+    CONFIGSTUFF="CPPFLAGS=-DU_USE_STRTOD_L=0"
+elif [ $ICU4CMAJ = 59 ];
+then
+    # workaround for xlocale
+    CONFIGSTUFF="CPPFLAGS=-DU_USE_STRTOD_L=0"
+else
+    CONFIGSTUFF=
+fi
+
+echo configure options $CONFIGSTUFF $EXTRACONFIGSTUFF
+
+exec ${SHELL} /src/icu/icu4c/source/configure $CONFIGSTUFF $EXTRACONFIGSTUFF $* || (tail config.log ; exit 1)
+
