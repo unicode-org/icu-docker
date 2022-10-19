52dbe9b2a9cea3a7f9906c7e68833a1e7ba3a4c2
Craig Cornelius <ccornelius@google.com>
2022-10-18 18:32:39 -0700

    ICU-21958 Update Docker files for building distribution

--- src/bin/icu-configure.sh
+++ src/bin/icu-configure.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # © 2016 and later: Unicode, Inc. and others.
 # License & terms of use: http://www.unicode.org/copyright.html
+git config --global --add safe.directory /src/icu
 
 . /src/bin/icu-dev.sh
 exec ${SHELL} /src/icu/icu4c/source/configure $*
